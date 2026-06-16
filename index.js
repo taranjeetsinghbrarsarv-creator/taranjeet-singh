de0bf8401f51882da46b1ea8b328e120ba9bf40f
Taranjeet <taranjeetsinghbrarsarv@gmail.com>
2026-06-16 09:53:44 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-let slides = document.querySelectorAll(".slide");
-let index = 0;
-
-setInterval(() => {
-  slides[index].classList.remove("active");
-  index = (index + 1) % slides.length;
-  slides[index].classList.add("active");
-}, 3000);
